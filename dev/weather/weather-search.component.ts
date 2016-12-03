978140be41f1fdfe8fef1613a0e0f9c99b610e4e
Mani vasu Jakka <mjakka@tibco.com>
2016-12-03 14:44:11 +0530

    first commit

---
+++ dev/weather/weather-search.component.ts
@@ -0,0 +1,39 @@
+import {Component} from "angular2/core";
+import {ControlGroup} from "angular2/src/common/forms/model";
+import {WeatherService} from "./weather.service";
+import {subscribeOn} from "rxjs/operator/subscribeOn";
+import {WeatherItem} from "./weather-item";
+@Component({
+    selector:'weather-search',
+    template:`
+    <section class="weather-search">
+            <form (ngSubmit)="onSubmit(f)" #f="ngForm">
+                <label for="city">City</label>
+            <input ngControl="location" type="text" id="city" required>
+            <button type="submit">Add City</button>
+            </form>
+        <div>
+            <span class="info">City found:</span>City Name
+        </div>
+    </section>`,
+    providers: [WeatherService]
+
+})
+
+export class WeatherSearchComponent {
+    constructor (private _weatherService:WeatherService){};
+
+    onSubmit(form:ControlGroup){
+  this._weatherService.searchWeatherData(form.value.location)
+        .subscribe(
+            data => {
+                const weatherItem =  new WeatherItem(data.name,data.weather[0]
+                    .description, data.main.temp);
+
+                this._weatherService.addWeatherItem(weatherItem);
+
+            }
+        )
+
+    }
+}
